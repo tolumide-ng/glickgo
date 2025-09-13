4c212e99ba901f18b4b9d1da102494bb138c36b4
tolumide-ng <tolumideshopein@gmail.com>
2025-09-13 23:53:27 +0200

    Initial commit

---
+++ glickgo.go
@@ -0,0 +1,9 @@
+package glickgo
+
+// Glickgo is a glicko-2 library in golang
+
+import "fmt"
+
+func Greet(name string) string {
+	return fmt.Sprintf("Hello %s!", name)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tolumide-ng/glickgo
+
+go 1.24.0
